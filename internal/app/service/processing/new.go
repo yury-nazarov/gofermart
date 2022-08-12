a00e3dec45ecd5f3d971f72a9d5402bfad8b6e84
Yury <yury.s.nazarov@gmail.com>
2022-08-12 08:54:26 +0300

    fix: [order] uploaded_at json date

--- internal/app/service/processing/new.go
+++ internal/app/service/processing/new.go
@@ -93,7 +93,7 @@ func (o orderStruct) orderConvertData(orderList []pg.OrderDB) (clearOrderList []
 
 		// Форматируем в: "2020-12-10T15:15:45+03:00"
 		// 				   2006-01-02T15:04:05Z07:00
-		order.UploadedAt = newOrderTime.In(loc).Format("2006-01-2T15:4:5Z07:00")
+		order.UploadedAt = newOrderTime.In(loc).Format("2006-01-2T15:04:05Z07:00")
 
 		// TODO: Пока не переложил в новый слайс, новое значение не возвращалось. Разобратся в чем проблема!
 		// Добавляем результат в новый слайс

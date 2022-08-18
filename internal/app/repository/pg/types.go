e29ff6a022bcdfe98423ac826bde090c78b918ff
Yury <yury.s.nazarov@gmail.com>
2022-08-18 16:16:04 +0300

    fix: [clean] delete debug

--- internal/app/repository/pg/types.go
+++ internal/app/repository/pg/types.go
@@ -23,7 +23,6 @@ type WithdrawDB struct {
 // DBInterface методы работы с релиационными БД
 type DBInterface interface {
 	Ping() bool
-	GetToken(ctx context.Context, token string) (bool, error)
 	// UserExist проверяет наличие пользователя в БД
 	UserExist(ctx context.Context, login string) (bool, error)
 	NewUser(ctx context.Context, login string, pwd string) (int, error)
